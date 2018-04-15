857f533d1c4bb9c193aae76743a6c8634d13af25
Adam McCarthy <B00085376@student.itb.ie>
2018-04-15 21:23:21 +0100

    Added a way for users to review Monster Original

--- monsterOriginal.php
+++ monsterOriginal.php
@@ -1,4 +1,15 @@
 <?php
+if($_POST){
+    $name = $_POST['name'];
+    $summary = $_POST['commentContent'];
+    $date = $_POST['date'];
+    $location = $_POST['location'];
+    $pricePaid = $_POST['price'];
+    $rating = $_POST['rating'];
+    $handle = fopen("monsterReview.html","a");
+    fwrite($handle, "<b>" . $name . ' (' . $date . ') ' . "(" . $rating . " stars) " . "</b>:<br/>" . "<b>Retailer(s): </b>" . $location . "<br>" . "<b>Price paid: </b>" . $pricePaid . "<br>" . $summary . "<br/>");
+    fclose($handle);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +49,15 @@
         <p>Carbonated water, sucrose, glucose, citric acid, natural flavors, taurine, sodium citrate,color added, panax ginseng root extract, L-carnitine, caffeine, sorbic acid, benzoic acid, niacinamide, sodium chloride, Glycine max glucuronolactone, inositol, guarana seed extract, pyridoxine hydrochloride, sucralose, riboflavin, maltodextrin, and cyanocobalamin</p>
     </div>
 </section>
-
-
+<form action="" method="POST">
+    Comment: <textarea rows="10" cols="30" name="commentContent"></textarea><br>
+    Name: <input type="text" name="name" placeholder="Name"><br>
+    Date: <input type="date" name="date" placeholder="31/12/2000"><br>
+    Retailer(s): <input type="text" name="location" placeholder="Super Valu"><br>
+    Price paid: <input type="number" min="0.00" max="100.00" step="0.01" name="price" placeholder="1.50"><br>
+    Rating: <input type="number" min="1" max="5" name="rating" placeholder="4">
+    <input type = "submit" value="Post!"><br/>
+</form>
+<?php include "monsterReview.html"; ?>
 </body>
 </html>
